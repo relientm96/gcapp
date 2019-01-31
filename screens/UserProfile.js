c97031a43ac022fb71498c73cf605f0530c45a53
Matthew <matthewyfy@gmail.com>
2019-01-31 20:55:56 +0800

    User Profile for navigation updated

--- screens/UserProfile.js
+++ screens/UserProfile.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 class UserProfile extends React.Component {
     render(){
