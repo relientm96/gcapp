438e47dee1fc2aaedfce22087ade0153968ae19d
Matthew <matthewyfy@gmail.com>
2019-02-02 17:48:34 +0800

    Rough Card View

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar} from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 
-import { Container, Content, Header, Left, Body, Icon, Title, Button} from 'native-base';
+import { Container, Content, Header, Left, Body, Icon, Title, Button, Card, CardItem} from 'native-base';
 
 class MainScreen extends React.Component {
     constructor() {
@@ -58,10 +58,34 @@ class MainScreen extends React.Component {
                 <Content contentContainerStyle={{
                     flex: 1, 
                     alignItems: 'center',
-                    justifyContent: 'center',
                 }}>
 
-                    <Text>Main Screen!</Text>
+                <View style={{flex: 2, alignSelf: 'stretch' }}>
+
+                    <Card style={{flex:1, alignItems: 'center'}}>
+                        <CardItem>
+                            <Text>
+                                CardItem #1
+                            </Text>
+                        </CardItem>
+                        <CardItem cardBody>
+                            <Image source={{uri: 'https://images.unsplash.com/photo-1425913397330-cf8af2ff40a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'}} style={{height: 150, width: null, flex: 1}}/>
+                        </CardItem>
+                    </Card>
+                    
+                </View>
+
+                <View style={{flex: 1, alignSelf: 'stretch' }}>
+
+                    <Card style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+                        <CardItem>
+                            <Text>
+                            Matthew 5:16: In the same way, let your light shine before others, that they may see your good deeds and glorify your Father in heaven.
+                            </Text>
+                        </CardItem>
+                    </Card>
+
+                </View>
 
                 </Content>
 
