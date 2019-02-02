3cefb6ff1aba9c40c3a3c8fde70ea79dbf0cf3d8
Matthew <matthewyfy@gmail.com>
2019-02-02 17:57:12 +0800

    Front Page rough layout done

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -49,8 +49,9 @@ class MainScreen extends React.Component {
 
             </Header>
 
-            <View style={{flex: 1, backgroundColor:'rgba(123, 167, 204, 1)'}}>
-
+            <View style={{flex: 1, backgroundColor:'rgba(123, 167, 204, 1)', justifyContent:'center', alignItems:'center'}}>
+                <Image style={{height: 100, width: 100, borderRadius: 50}} source={{uri: 'https://scontent.fkul8-1.fna.fbcdn.net/v/t1.0-9/16196015_10154888128487744_6901111466535510271_n.png?_nc_cat=103&_nc_ht=scontent.fkul8-1.fna&oh=0b30d1f322e3ca702c6b211b910ed6ce&oe=5CF4CDE9'}}/>
+                <Text styles={{color:'#ffffff'}}> Matthew Yong </Text>
             </View>
 
             <View style={{flex: 2, backgroundColor:'powderblue'}}>
