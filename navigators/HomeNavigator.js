75e4e6c0f6195fa65fdb73bf0b804c5e75c6ce42
Matthew <matthewyfy@gmail.com>
2019-02-03 17:36:03 +0800

    Removed nativebase components

--- navigators/HomeNavigator.js
+++ navigators/HomeNavigator.js
@@ -1,22 +1,45 @@
+import React from 'react'
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
-
+import { createMaterialTopTabNavigator} from 'react-navigation'
+import Icon from 'react-native-vector-icons/Ionicons';
 import UserProfile from '../screens/UserProfile';
 import MainScreen from '../screens/MainScreen';
 
-const HomeNavigator = createMaterialBottomTabNavigator(
+const HomeNavigator = createMaterialTopTabNavigator(
     { 
         Main: {
             screen: MainScreen,
+            navigationOptions:{
+                tabBarLabel: 'Home',
+                tabBarIcon: ({tintColor})=>(
+                    <Icon name="ios-home" color={tintColor} size={24}/>
+                )
+            }
         },
         User: {
             screen: UserProfile,
+            navigationOptions:{
+                tabBarLabel: 'Profile',
+                tabBarIcon: ({tintColor})=>(
+                    <Icon name="ios-person" color={tintColor} size={24}/>
+                )
+            }
         } 
     },
     {
         initialRouteName: 'Main',
-        headerMode: 'none',
-        navigationOptions: {
-            headerVisible: false,
+        order:['Main','User'],
+        tabBarOptions: {
+            activeTintColor: 'skyblue',
+            inactiveTintColor: 'grey',
+            style: {
+                backgroundColor: 'steelblue'
+            },
+            indicatorStyle: {
+                height: 0
+            },
+            showIcon: true
+
         }
     }
 );
--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView} from 'react-native';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
-import { Container, Content, Header, Left, Body, Icon, Title, Button, Card, CardItem, Footer} from 'native-base';
 
 import MyFooter from '../components/myFooter'
 
-
 class MainScreen extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-        isReady: false
-        };
+    constructor(props) {
+        super(props);
+        this.state = { loading: true };
     }  
 
     async componentWillMount() {
@@ -20,78 +16,22 @@ class MainScreen extends React.Component {
             Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
             Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
         });
-        this.setState({ isReady: true });
+        this.setState({ loading: false });
     }
 
     render(){
 
-        if (!this.state.isReady) {
+        if (this.state.loading) {
             return <Expo.AppLoading />;
         }
 
         return (
 
-        <Container style={{backgroundColor:'skyblue'}}>
+        <View style={{backgroundColor:'skyblue'}}>
 
-            <View style={{height: getStatusBarHeight(), backgroundColor: 'rgba(46, 85, 118, 1)'}}></View>
+            <Text> Main </Text>
 
-            <Header style={{backgroundColor: 'steelblue'}}>
-
-                <Body style={{justifyContent:'center', alignItems:'center'}}>
-                    <Title >Gospel Community</Title>
-                </Body>
-
-            </Header>
-
-            <View style={{flex: 1, justifyContent:'center', alignItems:'center'}}>
-                <Card style={{flex:1, alignItems: 'center', alignSelf: 'stretch'}}>
-                        <CardItem>
-                            <Text>
-                                CardItem #1
-                            </Text>
-                        </CardItem>
-                </Card>
-            </View>
-
-            <View style={{flex: 2}}>
-
-                <Content contentContainerStyle={{
-                    flex: 1, 
-                    alignItems: 'center',
-                }}>
-
-                <View style={{flex: 2, alignSelf: 'stretch' }}>
-
-                    <Card style={{flex:1, alignItems: 'center'}}>
-                        <CardItem>
-                            <Text>
-                                CardItem #2
-                            </Text>
-                        </CardItem>
-                        <CardItem cardBody>
-                            <Image source={{uri: 'https://images.unsplash.com/photo-1425913397330-cf8af2ff40a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80'}} style={{height: 150, width: null, flex: 1}}/>
-                        </CardItem>
-                    </Card>
-                    
-                </View>
-
-                <View style={{flex: 1, alignSelf: 'stretch' }}>
-
-                    <Card style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
-                        <CardItem>
-                            <Text>
-                            Matthew 5:16: In the same way, let your light shine before others, that they may see your good deeds and glorify your Father in heaven.
-                            </Text>
-                        </CardItem>
-                    </Card>
-
-                </View>
-
-                </Content>
-
-            </View>
-
-        </Container>
+        </View>
 
         );
     }
