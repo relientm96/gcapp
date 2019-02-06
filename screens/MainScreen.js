a123aab6da04ad18244302ecdff60772005284a6
Matthew <matthewyfy@gmail.com>
2019-02-06 21:19:14 +0800

    Sorted font loading issues

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -3,81 +3,103 @@ import { StyleSheet, View, Image, SafeAreaView} from 'react-native';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
 import { Container, Header, Content, Card, CardItem, Body, Text, Title } from 'native-base';
 
+import { Font, AppLoading } from 'expo';
+
 class MainScreen extends React.Component {
     constructor(props) {
         super(props);
+        this.state = { 
+            loading: true 
+        };
     }  
+
+    async componentWillMount() {
+       await Font.loadAsync({
+          Roboto: require("native-base/Fonts/Roboto.ttf"),
+          Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf")
+        });
+        this.setState({ loading: false });
+    };
 
     render(){
 
-        return (
-        <SafeAreaView style={{ flex:1, backgroundColor: 'white' }}>  
-
-            <View style={{height:getStatusBarHeight(), backgroundColor: 'steelblue'}}/> 
-
-            <View style={{flex:1}}>
-
-                <View style={{flex:2}} >
-                    <Container>
-                        <Header>
-                            <Body>
-                                <Text>This week's study</Text>
-                            </Body>
-                        </Header>
-                        <Content padder>
-                        <Card>
-                            <CardItem header bordered>
-                            <Text>Gospel of John</Text>
-                            </CardItem>
-                            <CardItem bordered>
-                            <Body>
-                                <Text>
-                                Though it is one of easiest books to read, 
-                                John is also one of the most profound books to understand. 
-                                Let veteran Bible teacher and pastor, Dr. J. Vernon McGee 
-                                lead you through the entire book in these 21 summaries 
-                                from Thru the Bible’s tried and trusted five year study.
-                                </Text>
-                            </Body>
-                            </CardItem>
-                            <CardItem footer bordered>
-                            <Text>Matthew Yong - 6/2/2019</Text>
-                            </CardItem>
-                        </Card>
-                        </Content>
-                    </Container>
+        if ( this.state.loading ){
+            return (
+                <View>
+                    <AppLoading />                
                 </View>
+            )
+        }
+        else {
+            return (
+            <SafeAreaView style={{ flex:1, backgroundColor: 'white' }}>  
+
+                <View style={{height:getStatusBarHeight(), backgroundColor: 'steelblue'}}/> 
 
                 <View style={{flex:1}}>
-                <Container>
-                        <Header>
-                            <Body>
-                                <Title>Verse of the day</Title>
-                            </Body>
-                        </Header>
-                        <Content padder>
-                        <Card>
-                            <CardItem header bordered>
-                                <Text>Matthew 18:15</Text>
-                            </CardItem>
-                            <CardItem bordered>
-                            <Body>
-                                <Text>
-                                “If your brother or sister sins, go and point out their fault, just between the two of you. 
-                                If they listen to you, you have won them over.
-                                </Text>
-                            </Body>
-                            </CardItem>
-                        </Card>
-                        </Content>
-                    </Container>
+
+                    <View style={{flex:2}} >
+                        <Container>
+                            <Header>
+                                <Body>
+                                    <Title>This week's study</Title>
+                                </Body>
+                            </Header>
+                            <Content padder>
+                            <Card>
+                                <CardItem header bordered>
+                                <Text>Gospel of John</Text>
+                                </CardItem>
+                                <CardItem bordered>
+                                <Body>
+                                    <Text>
+                                    Though it is one of easiest books to read, 
+                                    John is also one of the most profound books to understand. 
+                                    Let veteran Bible teacher and pastor, Dr. J. Vernon McGee 
+                                    lead you through the entire book in these 21 summaries 
+                                    from Thru the Bible’s tried and trusted five year study.
+                                    </Text>
+                                </Body>
+                                </CardItem>
+                                <CardItem footer bordered>
+                                <Text>Matthew Yong - 6/2/2019</Text>
+                                </CardItem>
+                            </Card>
+                            </Content>
+                        </Container>
+                    </View>
+
+                    <View style={{flex:1}}>
+                    <Container>
+                            <Header>
+                                <Body>
+                                    <Title>Verse of the day</Title>
+                                </Body>
+                            </Header>
+                            <Content padder>
+                            <Card>
+                                <CardItem header bordered>
+                                    <Text>Matthew 18:15</Text>
+                                </CardItem>
+                                <CardItem bordered>
+                                <Body>
+                                    <Text>
+                                    “If your brother or sister sins, go and point out their fault, just between the two of you. 
+                                    If they listen to you, you have won them over.
+                                    </Text>
+                                </Body>
+                                </CardItem>
+                            </Card>
+                            </Content>
+                        </Container>
+                    </View>
+
+
                 </View>
 
-
-            </View>
-
-        </SafeAreaView>
-        );
+            </SafeAreaView>
+            );
+        }
     }
 }
 
